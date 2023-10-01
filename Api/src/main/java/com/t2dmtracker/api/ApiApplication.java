f9592f0e51d6d1383b8ee99d19a8355195d96caf
Frederic Picard <fred.picard@gmail.com>
2023-10-01 16:30:16 +0200

    Initial commit

---
+++ Api/src/main/java/com/t2dmtracker/api/ApiApplication.java
@@ -0,0 +1,13 @@
+package com.t2dmtracker.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiApplication.class, args);
+	}
+
+}
---
+++ Api/src/test/java/com/t2dmtracker/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.t2dmtracker.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
