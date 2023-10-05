5f179db155780d1ae6b651228b0eb120c32f5a47
Frederic Picard <fred.picard@gmail.com>
2023-10-05 15:05:30 +0200

    GW : - Created basic route

--- GW/src/main/java/com/t2dmtracker/gw/GwApplication.java
+++ GW/src/main/java/com/t2dmtracker/gw/GwApplication.java
@@ -2,6 +2,9 @@ package com.t2dmtracker.gw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GwApplication {
@@ -10,4 +13,12 @@ public class GwApplication {
 		SpringApplication.run(GwApplication.class, args);
 	}
 
+	@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(p -> p.host("localhost:8080")
+						.and()
+						.uri("http://localhost:9001"))
+				.build();
+	}
 }
