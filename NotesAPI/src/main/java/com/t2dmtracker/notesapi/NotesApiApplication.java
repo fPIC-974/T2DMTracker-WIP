0c084fed0afbd0e9ccaed2770b944fe852bf7eef
Frederic Picard <fred.picard@gmail.com>
2023-10-05 18:04:36 +0200

    NotesAPI : - Created service

--- NotesAPI/src/main/java/com/t2dmtracker/notesapi/NotesApiApplication.java
+++ NotesAPI/src/main/java/com/t2dmtracker/notesapi/NotesApiApplication.java
@@ -1,13 +1,22 @@
 package com.t2dmtracker.notesapi;
 
+import com.t2dmtracker.notesapi.service.NoteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NotesApiApplication {
+public class NotesApiApplication implements CommandLineRunner {
+
+	@Autowired
+	private NoteService noteService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(NotesApiApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+	}
 }
---
+++ NotesAPI/src/main/java/com/t2dmtracker/notesapi/service/NoteService.java
@@ -0,0 +1,30 @@
+package com.t2dmtracker.notesapi.service;
+
+import com.t2dmtracker.notesapi.model.Note;
+import com.t2dmtracker.notesapi.repository.NoteRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class NoteService {
+    private final Logger logger = LoggerFactory.getLogger(NoteService.class);
+
+    private final NoteRepository noteRepository;
+
+    public NoteService(NoteRepository noteRepository) {
+        this.noteRepository = noteRepository;
+    }
+
+    public List<Note> getNotesByPatient(String id) {
+        logger.debug("Calling getNotesByPatient(" + id + ")");
+
+        List<Note> notes = noteRepository.findByPatientId(id);
+
+        logger.debug(notes.toString());
+
+        return notes;
+    }
+}
